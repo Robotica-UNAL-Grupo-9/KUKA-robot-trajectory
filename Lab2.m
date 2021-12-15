37f68adfbf1983e9dad31f9d6764449210d31a1e
Sebastian <jsduenass@unal.edu.co>
2021-12-15 15:02:39 -0500

    [FIX] orientation angles change DH modified parameters

--- Lab2.m
+++ Lab2.m
@@ -4,15 +4,29 @@ clc, clear ,close all
 
 L1=0.340; L3=0.400; L5=0.400; L7=0.161;
 
+% theta_i d_i a_(i-1) alpha_(i-1) [rad]
 
-L(1) = Link('revolute'   ,'alpha',      0,  'a',  0,     'd',    L1 , 'offset',     0, 'qlim',  [-170*pi/180 170*pi/180],   'modified');
-L(2) = Link('revolute'  ,'alpha',   pi/2,  'a',  0, 'd',    0 , 'offset',    0, 'qlim',       [-120*pi/180 120*pi/180],   'modified');
-L(3) = Link('revolute'   ,'alpha',  -pi/2,  'a',  0,     'd',    L3 , 'offset',     0, 'qlim',  [-170*pi/180 170*pi/180],   'modified');
-L(4) = Link('revolute'   ,'alpha',  pi/2,  'a',  0,     'd',    0 , 'offset',     0, 'qlim',  [-120*pi/180 120*pi/180],   'modified');
-L(5) = Link('revolute'   ,'alpha',  -pi/2,  'a',  0,     'd',    L5, 'offset',     0, 'qlim',  [-170*pi/180 170*pi/180],   'modified');
-L(6) = Link('revolute'   ,'alpha',  pi/2,  'a',  0,     'd',    0 , 'offset',     0, 'qlim',  [-120*pi/180 120*pi/180],   'modified');
-L(7) = Link('revolute'   ,'alpha',  -pi/2,  'a',  0,     'd',    L7 , 'offset',     0, 'qlim',  [-175*pi/180 175*pi/180],   'modified');
+DH_mod=[0 L1 0 0 ;
+        0  0 0 -pi/2;
+        0 L3 0  pi/2;
+        0  0 0 -pi/2;
+        0 L5 0  pi/2;
+        0  0 0 -pi/2;
+        0 L7 0  pi/2];
 
+      
+offset=[0 0 0 0 0 0 0];
+
+qlim=[ -170 170;       -120 120;
+       -170 170;       -120 120;
+       -170 170;       -120 120;
+       -175 1750];
+     
+for k=1:length(offset)
+  
+  L(k)=Link(DH_mod(k,:),'offset',offset(k),'qlim',qlim(k,:),'modified');
+end     
+  
 ws=[-5 2 -4 4 -2 5];
 
 plot_options = {'workspace',ws,'scale',.4,'view',[125 25],'jaxes','basewidth',10};
@@ -20,7 +34,7 @@ RKuka = SerialLink(L,'name','Kuka','plotopt',plot_options)
 
 %Determinar la posición del último sistema coordenado dado q:
 % q3=0
-q=[pi/3 pi/6 0 pi/4 -pi/3 3*pi/4 pi/4];
+q=[pi/3 pi/6 0 pi/4 -pi/3 3*pi/4 pi/8];
 
 MTH=RKuka.fkine(q)
 
@@ -42,7 +56,7 @@ beta=atan2(s_beta,sqrt(1-s_beta^2));
 gamma=atan2(r,z-L1);
 
 q3=0;
-q1=atan2(-y,-x);
+q1=atan2(y,x);
 q4=atan2(s4,c4);
 
 q2=-beta+gamma;
@@ -56,8 +70,8 @@ Twrist=RKuka.fkine(q_calc);
 
 R36=inv(Rwrist)*R;
 
-angles=tr2eul(R36,'deg')  % Phi, Theta Psi z,x,y
-q_567_alt=tr2eul(R36);
+angles=tr2eul(R36,'flip','deg')  % Phi, Theta Psi z,x,y
+q_567=tr2eul(R36);
 
 c6=R36(3,3);
 
@@ -66,16 +80,21 @@ q5=atan2(R36(2,3),R36(1,3));
 q6=atan2(sqrt(1-c6^2),c6);
 q7=atan2(R36(3,2),-R36(3,1));
 
-q_567=[q5 q6 q7] ;
+%q_567=[q5 q6 q7] ;
 
 q_calc=[q1,q2,q3,q4,q_567];
 
-q*180/pi
-q_calc*180/pi
+disp("q:  " )
+disp(q*180/pi)
+
+disp("q_calc")
+disp(q_calc*180/pi)
+
+
 
 Tcalc=RKuka.fkine(q_calc);
 
-e=Tcalc -MTH
+e=round(Tcalc -MTH,10)
 
 var=RKuka.fkine([q(1:4), 0 0 0]);
 
