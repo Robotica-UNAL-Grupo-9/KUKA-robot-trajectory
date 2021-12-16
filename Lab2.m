3ee052ab16b11899acd7a741abdc9d1ce1d9d06e
Juan Sebastian M <jsmojicav@unal.edu.co>
2021-12-15 23:43:05 -0500

    Update Lab2.m

--- Lab2.m
+++ Lab2.m
@@ -175,6 +175,72 @@ MTHikunc=RKuka.fkine(qikunc);%Verificacion MTH a partir de solucion ikunc
 qikcon=(RKuka.ikcon(MTH));%Cinematica inversa funcion okcon
 MTHikcon=RKuka.fkine(qikcon);%Verificacion MTH a partir de solucion ikcon
 
+%% Cinematica Inversa Toolbox RST
+clear variables
+clc
+MF=0.161;
+dhparams = [0   	0	0.340   	0;
+            0	pi/2       0       0
+            0	-pi/2	0.400	0;
+            0   	pi/2	0	0;
+            0       -pi/2	0.400   	0;
+            0       pi/2       0       0;
+            0       -pi/2       MF       0];
+RkukaRST = rigidBodyTree;     
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
+
+setFixedTransform(jnt1,dhparams(1,:),'mdh');
+body1.Joint = jnt1;
+
+addBody(RkukaRST,body1,'base')
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','revolute');
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+body5 = rigidBody('body5');
+jnt5 = rigidBodyJoint('jnt5','revolute');
+body6 = rigidBody('body6');
+jnt6 = rigidBodyJoint('jnt6','revolute');
+body7 = rigidBody('body7');
+jnt7 = rigidBodyJoint('jnt7','revolute');
+
+setFixedTransform(jnt2,dhparams(2,:),'mdh');
+setFixedTransform(jnt3,dhparams(3,:),'mdh');
+setFixedTransform(jnt4,dhparams(4,:),'mdh');
+setFixedTransform(jnt5,dhparams(5,:),'mdh');
+setFixedTransform(jnt6,dhparams(6,:),'mdh');
+setFixedTransform(jnt7,dhparams(7,:),'mdh');
+body2.Joint = jnt2;
+body3.Joint = jnt3;
+body4.Joint = jnt4;
+body5.Joint = jnt5;
+body6.Joint = jnt6;
+body7.Joint = jnt7;
+addBody(RkukaRST,body2,'body1')
+addBody(RkukaRST,body3,'body2')
+addBody(RkukaRST,body4,'body3')
+addBody(RkukaRST,body5,'body4')
+addBody(RkukaRST,body6,'body5')
+addBody(RkukaRST,body7,'body6')
+
+
+q=[0 -pi/3 0 pi/3 0 pi/3 0];%COnfiguracion 
+qRST = struct('JointName',{'jnt1','jnt2','jnt3','jnt4','jnt5','jnt6','jnt7'},'JointPosition',{q(1),q(2),q(3),q(4),q(5),q(6),q(7)});
+
+
+MTH_RST = getTransform(RkukaRST,qRST,'body7')
+
+ik = inverseKinematics('RigidBodyTree',RkukaRST);
+weights = [0.25 0.25 0.25 1 1 1];
+initialguess = RkukaRST.homeConfiguration;
+[configSol,solInfo] = ik('body7',MTH_RST,weights,initialguess)
+qsolRST = struct2table(configSol);
+qsolRST
+MTHinvRST=getTransform(RkukaRST,configSol,'body7')
+
 %% Pruebas
 coordinates=[ 0.4 0.6 0.5 30 20 45;
               -0.35 0.4 0.8 40 60 10;
