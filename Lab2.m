b271650f74d59a8cff9242e29af0cd1deea9c7a6
Wassau <dachaconr@unal.edu.co>
2021-12-15 22:33:09 -0500

    Update Lab2.m
    Pruebas

--- Lab2.m
+++ Lab2.m
@@ -140,6 +140,7 @@ hold on
 RKuka.plot([q_calc(1:n), zeros(1,7-n)].*t)
 
 
+<<<<<<< Updated upstream
 %% Cinematica Inversa Toolbox RCV
 clear variables
 syms q1 q2 q3 q4 q5 q6 q7 MF L_1 L_2 L_3 real
@@ -166,6 +167,92 @@ MTH=RKuka.fkine(q); %MTH punto de prueba
 
 qikcon=(RKuka.ikcon(MTH));
 MTHikcon=RKuka.fkine(qikcon);
+=======
+%% 5.2 (old)
+
+MF=0.161;
+dhparams = [0   	0	0.340   	0;
+            0	pi/2       0       0
+            0	-pi/2	0.400	0;
+            0   	pi/2	0	0;
+            0       -pi/2	0.400   	0;
+            0       pi/2       0       0;
+            0       -pi/2       MF       0];
+RkukaRST = rigidBodyTree;     
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
+
+setFixedTransform(jnt1,dhparams(1,:),'mdh');
+body1.Joint = jnt1;
+
+addBody(RkukaRST,body1,'base')
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','revolute');
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+body5 = rigidBody('body5');
+jnt5 = rigidBodyJoint('jnt5','revolute');
+body6 = rigidBody('body6');
+jnt6 = rigidBodyJoint('jnt6','revolute');
+body7 = rigidBody('body7');
+jnt7 = rigidBodyJoint('jnt7','revolute');
+
+setFixedTransform(jnt2,dhparams(2,:),'mdh');
+setFixedTransform(jnt3,dhparams(3,:),'mdh');
+setFixedTransform(jnt4,dhparams(4,:),'mdh');
+setFixedTransform(jnt5,dhparams(5,:),'mdh');
+setFixedTransform(jnt6,dhparams(6,:),'mdh');
+setFixedTransform(jnt7,dhparams(7,:),'mdh');
+body2.Joint = jnt2;
+body3.Joint = jnt3;
+body4.Joint = jnt4;
+body5.Joint = jnt5;
+body6.Joint = jnt6;
+body7.Joint = jnt7;
+addBody(RkukaRST,body2,'body1')
+addBody(RkukaRST,body3,'body2')
+addBody(RkukaRST,body4,'body3')
+addBody(RkukaRST,body5,'body4')
+addBody(RkukaRST,body6,'body5')
+addBody(RkukaRST,body7,'body6')
+
+%test=homeConfiguration(RkukaRST)
+q=[pi/3 pi/6 pi/2 pi/4 3*pi/4 3*pi/4 3*pi/4];
+qRST = struct('JointName',{'jnt1','jnt2','jnt3','jnt4','jnt5','jnt6','jnt7'},'JointPosition',{q(1),q(2),q(3),q(4),q(5),q(6),q(7)});
+
+%motionModel = taskSpaceMotionModel("RigidBodyTree",RkukaRST)
+transformRST = getTransform(RkukaRST,qRST,'body7')
+clf
+figure()
+show(RkukaRST,qRST);
+%% Pruebas
+coordinates=[ 0.4 0.6 0.5 30 20 45;
+              -0.35 0.4 0.8 40 60 10;
+              0.3 0.2 -0.1 -30 180 4;
+              0.4 0.5 0 30 45  10];
+
+CR1 = coordinates( 1, 4:6);
+CR1 = eul2tr(CR1);
+CR1(1:3 , 4) = coordinates(1:3,1)';
+% MTH = [ [0.3 0.5 0.5 0 ]' [0.3 0.3 0.4 0 ]' [0.2 0 0 0 ]' [0.2 0.2 0.3 1]' ]; 
+
+
+[Qa , e1 ] = inverse_k(CR1 ,RKuka)
+
+R1 = tr2eul(MTH(1:3,1:3));
+
+CR1 = coordinates( 1, 4:6);
+CR1 = eul2tr(CR1);
+CR1(1:3 , 4) = coordinates(1:3,1)';
+% MTH = [ [0.3 0.5 0.5 0 ]' [0.3 0.3 0.4 0 ]' [0.2 0 0 0 ]' [0.2 0.2 0.3 1]' ]; 
+
+
+[Qa , e1 ] = inverse_k(CR1 ,RKuka)
+
+R1 = tr2eul(MTH(1:3,1:3));
+>>>>>>> Stashed changes
 
 
 
@@ -232,3 +319,5 @@ Tcalc=RKuka.fkine(q_calc);
 e=round(Tcalc -MTH,10);
 
 end
+
+
